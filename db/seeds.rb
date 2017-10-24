89a30d1e2f91be73eb2073ca73a0af3fc184e527
Caleb Hiebert <calebkhiebert@gmail.com>
2017-10-24 13:00:29 -0500

    resource testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Province.create([
+                  { province_code: 'MB', gst: 0.05, pst: 0.07, hst: 0 },
+                  { province_code: 'AB', gst: 0.05, pst: 0, hst: 0 }
+                ])
+
+User.create(first_name: 'Caleb',
+            last_name: 'Hiebert',
+            email: 'calebkhiebert@gmail.com')
+
+Product.create(name: 'Generic Wooden Trinket',
+               description: 'Wood',
+               price: 1.55,
+               stock_quantity: 5)
