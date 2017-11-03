b0e1e33b96f6d35ed83211bdbdfba8828f481170
Caleb Hiebert <calebkhiebert@gmail.com>
2017-11-03 12:17:34 -0500

    product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index', as: 'user'
-  resource :users
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  get '/products', to: 'products#index'
 end
