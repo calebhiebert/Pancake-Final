013498d0682025861ba3548474d654d3870cafea
Caleb Hiebert <calebkhiebert@gmail.com>
2017-11-05 10:42:40 -0600

    fixed cors issue, chrome was caching cors header

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module Woodshop
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:8080'
+        resource '*', :headers => :any, :methods => [:get, :post, :options], :credentials => true
+      end
+    end
   end
 end
--- public/src/http-common.js
+++ public/src/http-common.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  baseURL: 'http://192.168.1.110:3000'
+  baseURL: 'http://192.168.1.110:3000',
+  withCredentials: true
 });
