cee06062566cda3ca0e257de08b85e4dc2f3118b
Caleb Hiebert <calebkhiebert@gmail.com>
2017-11-05 18:32:52 -0600

    remove unused views from router

--- public/src/router/index.js
+++ public/src/router/index.js
@@ -3,11 +3,9 @@ import Router from 'vue-router'
 import Home from '@/components/Home'
 import AdminProductCreate from '@/components/AdminProductCreate'
 import ProductView from '@/components/ProductView'
-import AdminPagesView from '@/components/AdminPagesView'
 import AdminPagesCreate from '@/components/AdminPagesCreate'
 import AdminPagesEdit from '@/components/AdminPagesEdit'
 import PageView from '@/components/PageView'
-import AdminProductView from '@/components/AdminProductsView'
 import AdminIndex from '@/components/AdminIndex'
 
 Vue.use(Router);
@@ -19,10 +17,8 @@ export default new Router({
     //Products
     {path: '/product/create', name: 'AdminProductCreate', component: AdminProductCreate},
     {path: '/product/:id', name: 'ProductView', component: ProductView},
-    {path: '/admin/products', name: 'AdminProductView', component: AdminProductView},
 
     //Pages
-    {path: '/admin/pages', name: 'AdminPagesView', component: AdminPagesView},
     {path: '/admin/pages/create', name: 'AdminPagesCreate', component: AdminPagesCreate},
     {path: '/admin/pages/:title', name: 'AdminPagesEdit', component: AdminPagesEdit},
     {path: '/page/:title', name: 'PageView', component: PageView},
