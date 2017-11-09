1e3b06770aa45827649ca5d08dffc1dfd78f9f09
Caleb Hiebert <calebkhiebert@gmail.com>
2017-11-08 19:50:53 -0600

    create register page

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,7 +1,8 @@
 class Province < ApplicationRecord
-  validates :gst, :pst, :hst, :province_code, presence: true
+  validates :gst, :pst, :hst, :province_code, :name, presence: true
   validates :gst, :pst, :hst, numericality: { greater_than_or_equal_to: 0 }
   validates :province_code, length: { is: 2 }
+  validates :name, length: { in: 2..25 }
 
   has_many :location
 end
--- public/src/components/Register.vue
+++ public/src/components/Register.vue
@@ -1,30 +1,30 @@
 <template>
-  <div class="ui container segment">
-    <form class="ui form">
+  <div class="ui clearing container segment">
+    <form class="ui form" :class="{ error: errors.any() }">
       <div class="field">
         <label>Name</label>
         <div class="two fields">
           <div class="field">
-            <input placeholder="First Name" name="first_name" v-model="first_name">
+            <input placeholder="First Name" name="first_name" v-model="first_name" v-validate="{required: true}">
           </div>
           <div class="field">
-            <input placeholder="Last Name" name="last_name" v-model="last_name">
+            <input placeholder="Last Name" name="last_name" v-model="last_name" v-validate="{required: true}">
           </div>
         </div>
       </div>
       <div class="field">
         <label>Email</label>
-        <input placeholder="Email" type="email" name="email" v-model="email">
+        <input placeholder="Email" type="email" name="email" v-model="email" v-validate="{required: true, email: true}">
       </div>
       <div class="field">
         <label>Password</label>
         <div class="two fields">
           <div class="field">
-            <input placeholder="Password" type="password" name="password" v-model="password">
+            <input placeholder="Password" type="password" name="password" v-model="password" v-validate="{required: true}">
           </div>
           <div class="field">
             <input placeholder="Password Confirmation" type="password" name="password_confirmation"
-                   v-model="password_confirmation">
+                   v-model="password_confirmation" v-validate="{required: true, confirmed: 'password'}">
           </div>
         </div>
       </div>
@@ -33,35 +33,64 @@
         <label>Address</label>
         <div class="fields">
           <div class="twelve wide field">
-            <input name="address" placeholder="Address" v-model="address">
+            <input name="address" placeholder="Address" v-model="address" v-validate="{required: true}">
           </div>
           <div class="four wide field">
-            <input name="postal_code" placeholder="Postal Code" v-model="postal_code">
+            <input name="postal_code" placeholder="Postal Code" v-model="postal_code"
+                   v-validate="{required: true, regex: /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i}">
           </div>
         </div>
       </div>
       <div class="field">
         <label>Province</label>
-        <select class="ui fluid dropdown">
+        <select class="ui fluid dropdown" v-model="province_code" name="province_code" v-validate="{required: true}">
           <option value="">Province</option>
+          <option v-for="province in provinces" :value="province.province_code">{{ province.name }}</option>
         </select>
       </div>
+      <div class="ui error message" v-if="errors.any()">
+        <ul>
+          <li v-for="error in errors.all()">{{ error }}</li>
+        </ul>
+      </div>
+      <button type="button" class="ui right floated primary button">Register</button>
     </form>
   </div>
 </template>
 <script>
+  import Vue from 'vue'
+  import VeeValidate from 'vee-validate'
+  import {HTTP} from '../http-common'
+  Vue.use(VeeValidate);
+
   export default {
     name: 'Register',
 
+    created() {
+      this.getProvinces()
+    },
+
     data() {
       return {
+        status: 'IDLE',
+        email_status: 'IDLE',
+        provinces: [],
         first_name: '',
         last_name: '',
         email: '',
         password: '',
         password_confirmation: '',
         address: '',
-        postal_code: ''
+        postal_code: '',
+        province_code: ''
+      }
+    },
+
+    methods: {
+      getProvinces() {
+        HTTP.get('/provinces')
+          .then(response => this.provinces = response.data)
+          .catch(err => console.log(err))
       }
     }
   }
