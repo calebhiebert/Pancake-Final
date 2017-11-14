6f91c2db681d740e6ac5e1ba5acbbb8af4894e3e
Caleb Hiebert <calebkhiebert@gmail.com>
2017-11-14 10:53:31 -0600

    billing display

--- public/src/components/Checkout.vue
+++ public/src/components/Checkout.vue
@@ -19,21 +19,40 @@
     <div class="ui message" v-if="!loggedin">
       Please log in to continue
     </div>
-    <div class="ui segment" v-if="billing">
-      This will contain a summary of the order
-    </div>
+    <table class="ui celled padded table" v-if="billing">
+      <thead>
+      <tr>
+        <th>Product</th>
+        <th>Quantity</th>
+        <th>Price</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in cart">
+        <td>{{ item.product.name }}</td>
+        <td>{{ item.quantity }}</td>
+        <td>{{ (item.product.price * item.quantity).toFixed(2) }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <button class="ui right floated labeled green button">
+      Pay Now
+    </button>
   </div>
 </template>
 <script>
   import {EventBus, Me} from '../EventBus'
+  import {HTTP} from '../http-common'
 
   export default {
     name: 'Checkout',
 
     data() {
       return {
+        cart: [],
         loggedin: false,
-        billing: false
+        billing: false,
+        loading: true
       }
     },
 
@@ -43,9 +62,22 @@
       }
     },
 
+    methods: {
+      loadCart() {
+        this.loading = true;
+        HTTP.get('/cart')
+          .then(response => {
+            this.cart = response.data;
+            this.loading = false
+          })
+          .catch(err => console.log(err))
+      },
+    },
+
     mounted() {
       this.loggedin = Me.me !== null;
-      EventBus.$on('me-updated', () => this.loggedin = Me.me !== null)
+      EventBus.$on('me-updated', () => this.loggedin = Me.me !== null);
+      this.loadCart()
     }
   }
 </script>
