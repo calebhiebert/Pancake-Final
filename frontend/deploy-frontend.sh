2fe98abbe6f707f5d9360ca2e69bee2c1b67ee08
Caleb Hiebert <calebkhiebert@gmail.com>
2017-11-14 19:09:07 -0600

    attempting to figure out vps hosting in containers

---
+++ frontend/deploy-frontend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker build -t panchem/woodshop-frontend .
+
+docker stop woodshop-frontend
+docker rm woodshop-frontend
+
+docker run \
+    --name woodshop-frontend \
+    -d \
+    -p 80:80 \
+    -e VIRTUAL_PORT=80 \
+    -e VIRTUAL_HOST="woodshop.piikl.com" \
+    -e LETSENCRYPT_HOST="woodshop.piikl.com" \
+    -e LETSENCRYPT_EMAIL="info@piikl.com" \
+    panchem/woodshop-frontend
