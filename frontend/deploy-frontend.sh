9f13b088e458231e097be45c760583705d488ec7
Caleb Hiebert <calebkhiebert@gmail.com>
2017-11-14 22:44:02 -0600

    fix typo

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -10,7 +10,7 @@ docker run \
     -d \
     -p 8082:80 \
     -e VIRTUAL_PORT=80 \
-    -e VIRTUAL_HOST="woodshop.piikl.com,woodshop.fautline.piikl.com" \
+    -e VIRTUAL_HOST="woodshop.piikl.com,woodshop.faultline.piikl.com" \
     -e LETSENCRYPT_HOST="woodshop.piikl.com,woodshop.faultline.piikl.com" \
     -e LETSENCRYPT_EMAIL="info@piikl.com" \
     panchem/woodshop-frontend
